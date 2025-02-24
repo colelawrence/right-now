5f99903b647ab7ca731a2f3adb59c6b3663e62bc
Cole Lawrence <cole@colelawrence.com>
2025-02-24 12:33:42 -0300

    Optimize Rust linting in lint-staged config for performance
    Signed-off-by: Cole Lawrence <cole@colelawrence.com>

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,10 @@
+/** @type {import('lint-staged').Configuration} */
 export default {
   "*.{ts,tsx,css,html,mts,json}":
     "bunx biome check --fix --files-ignore-unknown=true --diagnostic-level=error --no-errors-on-unmatched",
-  "*.rs": ["cargo clippy --fix --allow-dirty --allow-staged", "cargo fmt --"],
+  "*.rs": (files) =>
+    // only spend more time if there are a lot of changes
+    files.length > 2
+      ? ["cargo clippy --fix --allow-dirty --allow-staged", "cargo fmt"]
+      : [`cargo fmt -- ${files.map((a) => `"${a}"`).join(" ")}`],
 };
