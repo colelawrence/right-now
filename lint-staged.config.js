fae3fecd750aa9a5b592389efa2236171aaf3e99
Cole Lawrence <cole@colelawrence.com>
2025-02-24 12:18:33 -0300

    Add Rust linting and formatting to lint-staged config
    Signed-off-by: Cole Lawrence <cole@colelawrence.com>

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,5 @@
 export default {
   "*.{ts,tsx,css,html,mts,json}":
     "bunx biome check --fix --files-ignore-unknown=true --diagnostic-level=error --no-errors-on-unmatched",
+  "*.rs": ["cargo clippy --fix --allow-dirty --allow-staged", "cargo fmt --"],
 };
